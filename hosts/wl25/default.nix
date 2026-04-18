65807216911291c2dbf39f9e14f13bcc34cf0c7f
mimiTsuki <49679298+mimiTsuki@users.noreply.github.com>
2026-04-18 13:49:45 +0900

    feat: wl25プロファイルではlibgccも管理するように

--- hosts/wl25/default.nix
+++ hosts/wl25/default.nix
@@ -1,4 +1,10 @@
-{ username, ... }:
+{ pkgs, username, ... }:
 {
   home.homeDirectory = "/home/${username}";
+  home.packages = with pkgs; [
+    libgcc
+  ];
+  home.sessionVariables = {
+    LD_LIBRARY_PATH = "${pkgs.libgcc}/lib";
+  };
 }
