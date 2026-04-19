471b2c319e0c2f4b099a4c3e166126d8970081cd
mimiTsuki <49679298+mimiTsuki@users.noreply.github.com>
2026-04-19 16:45:45 +0900

    feat: karabiner-elementsの設定ファイルを追加

--- darwin/home/karabiner/default.nix
+++ darwin/home/karabiner/default.nix
@@ -2,5 +2,7 @@
 {
   home.file = {
     ".config/karabiner/karabiner.json".source = ./karabiner.json;
+    ".config/karabiner/assets/complex_modifications/1768668749.json".source = ./assets/complex_modifications/1768668749.json;
+    ".config/karabiner/assets/complex_modifications/1768668771.json".source = ./assets/complex_modifications/1768668771.json;
   };
 }
