46959534811127b0423a3e369a9674107fce0117
mimiTsuki <49679298+mimiTsuki@users.noreply.github.com>
2026-06-02 15:27:27 +0900

    feat: starshipでawsプロファイルとk8s contextを表示するように設定を変更

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -12,6 +12,20 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      aws = {
+        disabled = false;
+        format = "[$symbol($profile)(\($region\))]($style) ";
+        symbol = "☁️  ";
+        style = "bold yellow";
+      };
+      kubernetes = {
+        disabled = false;
+        format = "[$symbol$context(\($namespace\))]($style) ";
+        symbol = "⎈ ";
+        style = "bold cyan";
+      };
+    };
   };
 
   programs.fzf = {
