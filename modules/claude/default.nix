2a3091e95ede4ef46733d8dc7fec4fb9df836ba7
mimiTsuki <49679298+mimiTsuki@users.noreply.github.com>
2026-08-21 15:54:00 +0900

    feat: ファイル提案スクリプトを追加

--- modules/claude/default.nix
+++ modules/claude/default.nix
@@ -3,5 +3,6 @@
   home.file = {
 #    ".claude/settings.json".source = ./settings.json;
     ".claude/statusline-command.sh".source = ./statusline-command.sh;
+    ".claude/file-suggestion.sh".source = ./file-suggestion.sh;
   };
 }
